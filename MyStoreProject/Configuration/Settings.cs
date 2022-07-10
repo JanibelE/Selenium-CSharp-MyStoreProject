7f4387950f37a44c9d0fb9c4ec3ad6d0e4909a34
janibel <janibelesp@gmail.com>
2022-07-10 16:56:32 -0400

    comentario

--- MyStoreProject/Configuration/Settings.cs
+++ MyStoreProject/Configuration/Settings.cs
@@ -6,6 +6,7 @@ namespace MyStoreProject.Configuration
 {
     public class Settings
     {
+        //get data from xml file
         public ConfigurationModel getSettings()
         {
             ConfigurationModel config = JsonConvert.DeserializeObject<ConfigurationModel>(File.ReadAllText(@"E:\Documentos\Proyectos\MyStoreProject\MyStoreProject\Configuration\Configuration.json"));
